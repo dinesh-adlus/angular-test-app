bddc6e993c3ef95bb69f5109028d16fb9bf0ea61
dinesh-adlus <Sruthi1995@>
2020-04-15 10:36:55 -0500

    updating changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
-import {TestingService} from './services';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '';
-  carcompany = [];
-  carddetails = [];
- constructor(public testingService: TestingService ) {
-   this.carcompany = this.testingService.cars;
-   this.testingService.getcardetails(this.carddetails).subscribe(carddetails = >{
+  loadedFeature = 'recipe';
 
-   });
- }
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
