5dce64ceb7bed15dbf4b3825a0351bb3f7fd28fa
dinesh-adlus <63668194+dinesh-adlus@users.noreply.github.com>
2021-06-17 16:58:05 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+rm -rf ./node_modules
 rm -rf ./dist
 rm -rf ./bundle
 rm -rf ./temp
@@ -8,4 +9,5 @@ rm -rf ./temp
 npm install
 
 echo "node modules are installed successfully"
+
 npm run build
